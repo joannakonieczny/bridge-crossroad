f01d65f04a00dafda3b335f901ed2c85818f9551
Bartłomiej Szubiak <bszubiak@student.agh.edu.pl>
2025-07-23 10:13:42 +0200

    typed_rotes done, not used

---
+++ next-app/src/routes.ts
@@ -0,0 +1,10 @@
+export const ROUTES = {
+  landing_page: "/",
+  auth: {
+    login: "/auth/login",
+    register: "/auth/register",
+    forgot_password: "/auth/forgot-password",
+  },
+  onboarding: "/onboarding",
+  dashboard: "/dashboard",
+};
