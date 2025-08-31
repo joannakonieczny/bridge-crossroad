38e94da2a2fac738871efda2d4c34f16dedf5380
Bartłomiej Szubiak <bszubiak@student.agh.edu.pl>
2025-08-31 20:41:54 +0200

    logo-darkmode

--- next-app/src/components/common/footer/FooterLogo.tsx
+++ next-app/src/components/common/footer/FooterLogo.tsx
@@ -1,29 +1,33 @@
 import Link from "next/link";
-import { Flex, Box } from "@chakra-ui/react";
-import Image from "next/image";
+import { Flex } from "@chakra-ui/react";
+import { ChakraSVG } from "@/components/chakra-config/ChakraSVG";
+import LogoDark from "@/assets/common/logo-darkmode.svg";
 import { ROUTES } from "@/routes";
 import { baseConfig } from "@/club-preset/baseConfig";
 import ResponsiveHeading from "../texts/ResponsiveHeading";
 
 export default function FooterLogo() {
-
   return (
     <Link href={ROUTES.dashboard} style={{ textDecoration: "none" }}>
-      <Flex gap={2} mr={100} cursor="pointer" alignItems="center">
-        <Box
-          minWidth={{ base: "3rem", md: "4rem" }}
-          minHeight={{ base: "2.77rem", md: "3.69rem" }}
-          position="relative"
-          marginRight={{base: "0.5rem", md: "1rem"}}
-        >
-          <Image
-            src="/assets/common/logo-darkmode.svg"
-            alt="Logo"
-            fill
-            style={{ objectFit: "contain", objectPosition: "left top" }}
-          />
-        </Box>
-        <ResponsiveHeading text={baseConfig.appName} showBar={false} fontSize="4xl" color="white" whiteSpace="nowrap"/>
+      <Flex
+        gap={2}
+        mr={100}
+        cursor="pointer"
+        alignItems="center"
+        rowGap={{ base: "0.5rem", md: "1rem" }}
+      >
+        <ChakraSVG
+          svg={LogoDark}
+          aria-label="Logo"
+          w={{ base: "3rem", md: "4rem" }}
+        />
+        <ResponsiveHeading
+          text={baseConfig.appName}
+          showBar={false}
+          fontSize="4xl"
+          color="white"
+          whiteSpace="nowrap"
+        />
       </Flex>
     </Link>
   );
