997cede57b8c41a2e86d1bb33634f4ac81f6bb7e
Bartłomiej Szubiak <bszubiak@student.agh.edu.pl>
2025-10-21 00:01:14 +0200

    transformEmptyStringsToUndefined used everywhere

--- next-app/src/schemas/common.ts
+++ next-app/src/schemas/common.ts
@@ -1,9 +1,5 @@
 import { z } from "zod";
 
-export function emptyStringToUndefined(value: string | undefined) {
-  return value === "" ? undefined : value;
-}
-
 export function isProbablyEmail(value: string) {
   return value.includes("@");
 }
