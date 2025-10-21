5c699d1513a6f6d3b0ee9f6056c4846d2920c135
Bartłomiej Szubiak <bszubiak@student.agh.edu.pl>
2025-10-21 20:03:24 +0200

    done refactor

--- next-app/src/util/envConfigLoader.ts
+++ next-app/src/util/envConfigLoader.ts
@@ -3,25 +3,48 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  SESSION_SECRET: z.string().default("123"),
-  EXPIRATION_TIME_SECONDS: z.coerce.number().default(3600),
-  SECURE_COOKIES: z.coerce.boolean().default(false),
+  SESSION_SECRET: z.string(),
+  EXPIRATION_TIME_SECONDS: z.coerce.number(),
+  SECURE_COOKIES: z.coerce.boolean(),
   MONGODB_URI: z.string(),
   MONGODB_DB_NAME: z.string(),
 });
 export type EnvVariables = z.infer<typeof envSchema>;
+
+const fallbackSecret = {
+  SESSION_SECRET: "123",
+  EXPIRATION_TIME_SECONDS: 3600,
+  SECURE_COOKIES: false,
+} satisfies Partial<EnvVariables>;
 
 //////
 // EXECUTION
 
 const environment = envSchema.safeParse(process.env);
 
-if (!environment.success) {
-  console.error("FATAL ERROR: Environment validation failed:");
-  environment.error.issues.forEach((issue) => {
-    console.error(`- ${issue.path.join(".")} : ${issue.message}`);
-  });
+const errors: string[] = [];
+const warnings: string[] = [];
+environment.error?.issues.forEach((issue) => {
+  const path = issue.path.join(".");
+  if (path in fallbackSecret) {
+    warnings.push(
+      `[ENV LOADER] WARNING : ${path} : using fallback value : ${
+        fallbackSecret[path as keyof typeof fallbackSecret]
+      }`
+    );
+  } else {
+    errors.push(`[ENV LOADER] FATAL ERROR : ${path} : ${issue.message}`);
+  }
+});
+
+warnings.forEach(console.warn);
+errors.forEach(console.error);
+if (errors.length > 0) {
+  console.error("[ENV LOADER] FATAL ERROR: Environment validation failed.");
   process.exit(1);
 }
 
-export const config: EnvVariables = environment.data;
+export const config: EnvVariables = envSchema.parse({
+  ...fallbackSecret,
+  ...process.env,
+});
