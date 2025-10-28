5e197f9f6554fefdebaad67a173a4d51b07959b4
Joanna Konieczny <konieczny.joasia@gmail.com>
2025-10-29 00:01:01 +0100

    Frontend draft

--- next-app/src/components/pages/with-onboarding/calendar/event-form/EventForm.tsx
+++ next-app/src/components/pages/with-onboarding/calendar/event-form/EventForm.tsx
@@ -9,8 +9,14 @@ import {
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useActionMutation } from "@/lib/tanstack-action/actions-mutation";
 import { useForm } from "react-hook-form";
-import { eventSchema } from "@/schemas/model/event/event-schema";
-import { useToast } from "@chakra-ui/react";
+import { addModifyEventSchema } from "@/schemas/pages/with-onboarding/events/events-schema";
+import {
+  TabList,
+  TabPanel,
+  TabPanels,
+  useToast,
+  VStack,
+} from "@chakra-ui/react";
 import { createEvent } from "@/services/events/api";
 import {
   ActionInput,
@@ -34,7 +40,21 @@ import {
   Textarea,
   Box,
   Spinner,
+  Tabs,
 } from "@chakra-ui/react";
+import {
+  Step,
+  StepDescription,
+  StepIcon,
+  StepIndicator,
+  StepNumber,
+  StepSeparator,
+  StepStatus,
+  StepTitle,
+  Stepper,
+  useSteps,
+} from "@chakra-ui/react";
+import { Divider } from "@chakra-ui/react";
 
 type EventFormProps = {
   isOpen: boolean;
@@ -49,15 +69,22 @@ export default function EventForm({ isOpen, onClose }: EventFormProps) {
     control: formControl,
     setError: setFormError,
   } = useForm({
-    resolver: zodResolver(eventSchema),
+    resolver: zodResolver(addModifyEventSchema),
     defaultValues: {
       title: "",
       description: "",
-      // ... other default values
+      location: "",
+      organizer: "",
+      duration: {
+        startsAt: new Date(),
+        endsAt: new Date(),
+      },
+      additionalDescription: "",
+      imageUrl: "",
+      data: {},
     },
   });
 
-  const router = useRouter();
   const toast = useToast();
 
   const createEventAction = useActionMutation({
@@ -78,19 +105,63 @@ export default function EventForm({ isOpen, onClose }: EventFormProps) {
     });
   }
 
+  function PrimaryInfoStep() {
+    return (
+      <Stack spacing={4}>
+        <FormLabel htmlFor="title">Tytuł</FormLabel>
+        <Input id="title" placeholder="Tytuł wydarzenia" />
+        <FormLabel htmlFor="description">Opis</FormLabel>
+        <Textarea id="description" placeholder="Opis wydarzenia" />
+        <FormLabel htmlFor="location">Lokalizacja</FormLabel>
+        <Input id="location" placeholder="Lokalizacja wydarzenia" />
+        <FormLabel htmlFor="organizer">Organizator</FormLabel>
+        <Input id="organizer" placeholder="Organizator wydarzenia" />
+        <FormLabel htmlFor="duration">Czas trwania</FormLabel>
+        <HStack spacing={4}>
+          <Input type="datetime-local" id="startsAt" />
+          <Input type="datetime-local" id="endsAt" />
+        </HStack>
+        <Button mt={4} colorScheme="blue">
+          Dalej
+        </Button>
+      </Stack>
+    );
+  }
+
+  const steps = [
+    { title: "Podstawowe informacje", content: <PrimaryInfoStep /> },
+    { title: "Szczegóły wydarzenia", content: <Box>Step 2 Content</Box> },
+    { title: "Podsumowanie", content: <Box>Step 3 Content</Box> },
+  ];
+
+  const { activeStep } = useSteps({
+    index: 0,
+    count: steps.length,
+  });
+
   return (
-    <Modal isOpen={isOpen} onClose={onClose} size="lg">
+    <Modal isOpen={isOpen} onClose={onClose} size="xl">
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>Dodaj wydarzenie</ModalHeader>
         <ModalCloseButton />
+        <Divider />
         <ModalBody>
-          <Stack spacing={4}>
-            <FormLabel htmlFor="title">Tytuł</FormLabel>
-            <Input id="title" placeholder="Tytuł wydarzenia" />
-            <FormLabel htmlFor="description">Opis</FormLabel>
-            <Textarea id="description" placeholder="Opis wydarzenia" />
-          </Stack>
+          <Stepper index={activeStep}>
+            {steps.map((step, index) => (
+              <Step key={index}>
+                <StepIndicator>
+                  <StepStatus
+                    complete={<StepIcon />}
+                    incomplete={<StepNumber />}
+                    active={<StepNumber />}
+                  />
+                </StepIndicator>
+              </Step>
+            ))}
+          </Stepper>
+          <Divider mt={4} />
+          <PrimaryInfoStep />
         </ModalBody>
         <ModalFooter>
           <Button colorScheme="blue">Dodaj wydarzenie</Button>
