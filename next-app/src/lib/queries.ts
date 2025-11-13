cd2cfc724db4c4309150a62bef7bd5db7bc19978
Bartłomiej Szubiak <bszubiak@student.agh.edu.pl>
2025-11-13 21:37:33 +0100

    done

--- next-app/src/lib/queries.ts
+++ next-app/src/lib/queries.ts
@@ -4,7 +4,12 @@ import { getJoinedGroupsInfo, getGroupData } from "@/services/groups/api";
 import { listEventsForUser, getEvent } from "@/services/events/api";
 import type { GroupIdType } from "@/schemas/model/group/group-types";
 import type { EventIdType } from "@/schemas/model/event/event-types";
-import type { TActionQueryOptionsHelper } from "./tanstack-action/types";
+import type {
+  ActionError,
+  TActionQueryOptionsHelper,
+} from "./tanstack-action/types";
+import { useToast } from "@chakra-ui/react";
+import { useGetMessageFromError } from "./tanstack-action/hooks-helpers";
 
 export const QUERY_KEYS = {
   joinedGroups: ["groups"],
@@ -16,12 +21,33 @@ export const QUERY_KEYS = {
   eventDetail: (id: EventIdType) => ["event", id],
 } as const;
 
+function useOnErrorToast(template: string, toastId: string) {
+  const toast = useToast();
+  const g = useGetMessageFromError(template);
+  const id = "q-error-toast" + toastId;
+
+  const helper = (e: ActionError) => {
+    if (toast.isActive(id)) return;
+    toast({
+      status: "error",
+      title: g(e),
+      id,
+    });
+  };
+  return helper;
+}
+
 export function useJoinedGroupsQuery(
   props?: TActionQueryOptionsHelper<typeof getJoinedGroupsInfo>
 ) {
+  const e = useOnErrorToast(
+    "grup w których jesteś administratorem",
+    "getJoinedGroupsInfo"
+  );
   return useActionQuery({
     queryKey: QUERY_KEYS.joinedGroups,
     action: getJoinedGroupsInfo,
+    onError: e,
     ...props,
   });
 }
@@ -30,9 +56,11 @@ export function useGroupQuery(
   groupId: GroupIdType,
   props?: TActionQueryOptionsHelper<typeof getGroupData>
 ) {
+  const e = useOnErrorToast("szczegółów grupy", "getGroupData");
   return useActionQuery({
     queryKey: QUERY_KEYS.groupDetail(groupId),
     action: () => getGroupData({ groupId }),
+    onError: e,
     ...props,
   });
 }
@@ -44,9 +72,11 @@ export function useEventsForUserQuery(
   },
   props?: TActionQueryOptionsHelper<typeof listEventsForUser>
 ) {
+  const e = useOnErrorToast("wydarzeń w kalendarzu", "listEventsForUser");
   return useActionQuery({
     queryKey: QUERY_KEYS.calendarEvents(timeWindow.start, timeWindow.end),
     action: () => listEventsForUser({ timeWindow }),
+    onError: e,
     ...props,
   });
 }
@@ -55,9 +85,11 @@ export function useEventQuery(
   eventId: EventIdType,
   props?: TActionQueryOptionsHelper<typeof getEvent>
 ) {
+  const e = useOnErrorToast("szczegółów wydarzenia", "getEvent");
   return useActionQuery({
     queryKey: QUERY_KEYS.eventDetail(eventId),
     action: () => getEvent({ eventId }),
+    onError: e,
     ...props,
   });
 }
