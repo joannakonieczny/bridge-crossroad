6bdc34c52bb91436cb626ce20997199cf49b930b
Szymon Kubiczek <125602027+Simsoftcik@users.noreply.github.com>
2025-12-14 13:40:26 +0100

    Feat - Mailer (#117)
    * mailer mocked template
    
    * connected gmail SMTP mailer
    
    * MailButton small clean
    
    * PR cleaning
    
    * PR fixes
    
    * fix async
    
    * updated workflow
    
    * revert package.json
    
    * package changes reaplied
    
    * added types for nodemailer
    
    ---------
    
    Co-authored-by: Bartłomiej Szubiak <bszubiak@student.agh.edu.pl>

---
+++ next-app/src/repositories/mailer.ts
@@ -0,0 +1,26 @@
+import nodemailer from "nodemailer";
+import { config } from "../util/envConfigLoader";
+
+const transporter = nodemailer.createTransport({
+  host: config.GMAIL_SMTP_HOST,
+  port: config.GMAIL_SMTP_PORT,
+  secure: false, // STARTTLS
+  auth: {
+    user: config.GMAIL_SMTP_USER,
+    pass: config.GMAIL_SMTP_PASSWORD,
+  },
+});
+
+type SendEmailInput = {
+  to: string;
+  subject: string;
+  body: string;
+};
+
+export const sendEmail = async ({ to, subject, body }: SendEmailInput) =>
+  transporter.sendMail({
+    from: config.GMAIL_FROM_EMAIL,
+    to,
+    subject,
+    html: body,
+  });
